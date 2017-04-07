4cc64627e73c34837107cb91c9d8e9394a4f8398
neodevelop <juan@makingdevs.com>
2017-04-07 12:30:22 -0500

    Making the test works with mocks

--- src/test/groovy/com/makingdevs/routes/FilterRouteTest.groovy
+++ src/test/groovy/com/makingdevs/routes/FilterRouteTest.groovy
@@ -3,9 +3,14 @@ package com.makingdevs.routes
 import org.apache.camel.EndpointInject
 import org.apache.camel.Produce
 import org.apache.camel.ProducerTemplate
+import org.apache.camel.builder.AdviceWithRouteBuilder
 import org.apache.camel.builder.RouteBuilder
 import org.apache.camel.component.mock.MockEndpoint
 import org.apache.camel.test.junit4.CamelTestSupport
+import org.junit.After
+import org.junit.AfterClass
+import org.junit.Before
+import org.junit.BeforeClass
 import org.junit.Test
 
 /**
@@ -13,18 +18,42 @@ import org.junit.Test
  */
 class InvoiceRouteTest extends CamelTestSupport {
 
-  @EndpointInject(uri = "mock:start")
+  @EndpointInject(uri = "mock:directInvoice")
   protected MockEndpoint resultEndpoint
 
   @Produce(uri = "direct:mail")
   protected ProducerTemplate template
+
+  @Override
+  protected RouteBuilder createRouteBuilder() {
+    new FilterRoute()
+  }
+
+  @Before
+  void mockEndpoints(){
+    AdviceWithRouteBuilder mockDirect = new AdviceWithRouteBuilder() {
+      @Override
+      void configure() throws Exception {
+        interceptSendToEndpoint("direct:obtainInvoice")
+            .skipSendToOriginalEndpoint()
+            .to("mock:directInvoice")
+      }
+    }
+    context.getRouteDefinition("filterMessage").adviceWith(context, mockDirect)
+    context.start()
+  }
+  @After
+  void stopCamelContext(){ context.stop() }
+
+  @Override
+  boolean isUseAdviceWith() { true }
 
   @Test
   void testSubjectContainsInvoice(){
     Map<String,String> headers = ["Subject":"FACTURA ABRIL"]
     resultEndpoint.expectedHeaderReceived("Subject", "FACTURA ABRIL")
     resultEndpoint.expectedMessageCount(4)
-    template.sendBodyAndHeaders("Any body", headers)
+    template.sendBodyAndHeaders("direct:mail", "Any body", headers)
 
     resultEndpoint.assertIsSatisfied()
 
@@ -48,8 +77,4 @@ class InvoiceRouteTest extends CamelTestSupport {
     resultEndpoint.assertIsSatisfied();
   }
 
-  @Override
-  protected RouteBuilder createRouteBuilder() {
-    new FilterRoute()
-  }
 }
