126345af4bbd1e5c139628a63296b43cfe21ffba
neodevelop <juan@makingdevs.com>
2017-04-10 10:50:23 -0500

    Adjusting the filter for messages with files

---
+++ src/main/groovy/com/makingdevs/routes/filters/MailFilter.groovy
@@ -0,0 +1,18 @@
+package com.makingdevs.routes.filters
+
+import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
+import org.apache.camel.Attachment
+import org.apache.camel.AttachmentObjects
+import org.apache.camel.Header
+
+@CompileStatic
+class MailFilter {
+  static boolean hasFilesFromAnInvoice(@AttachmentObjects Map<String, Attachment> attachments){
+    attachments.any { String fileName, data ->
+      fileName.contains(".xml")
+    } && attachments.any { String fileName, data ->
+      fileName.contains(".pdf")
+    }
+  }
+}
