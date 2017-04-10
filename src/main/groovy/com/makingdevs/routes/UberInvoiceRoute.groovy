7fec6867c57e0f92d080fc2f077ad9a40f392357
neodevelop <juan@makingdevs.com>
2017-04-10 11:40:04 -0500

    Creating the UberRoute

---
+++ src/main/groovy/com/makingdevs/routes/UberInvoiceRoute.groovy
@@ -0,0 +1,24 @@
+package com.makingdevs.routes
+
+import groovy.transform.CompileStatic
+import org.apache.camel.Exchange
+import org.apache.camel.builder.RouteBuilder
+import org.apache.camel.Message
+
+/**
+ * Created by makingdevs on 3/22/17.
+ */
+@CompileStatic
+class UberInvoiceRoute extends RouteBuilder {
+
+  void configure() {
+    from("file:download")
+    .process({ Exchange exchange ->
+      Message msg = exchange.getIn()
+      String newMessage = msg.getBody(String).find(/https:\/\/cfdi.uberfacturas.com\/downloadZIP[^"]*/).replace("https:","https4:")
+      msg.setBody(newMessage)
+    })
+    .toD('${body}?maxRedirects=3')
+    .to("log:groovymail?showFiles=true")
+  }
+}
