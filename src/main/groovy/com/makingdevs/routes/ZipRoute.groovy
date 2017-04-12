671dd9b8dc651adc35fa752dacc40784ac7e22e8
neodevelop <juan@makingdevs.com>
2017-04-12 15:19:53 -0500

    Adding the zip route

---
+++ src/main/groovy/com/makingdevs/routes/ZipRoute.groovy
@@ -0,0 +1,25 @@
+package com.makingdevs.routes
+
+import com.makingdevs.config.Application
+import com.makingdevs.routes.filters.MailFilter
+import org.apache.camel.Predicate
+import org.apache.camel.builder.RouteBuilder
+import org.apache.camel.dataformat.zipfile.ZipSplitter
+
+import static org.apache.camel.builder.PredicateBuilder.and
+import static org.apache.camel.builder.PredicateBuilder.or
+
+/**
+ * Created by makingdevs on 3/22/17.
+ */
+class ZipRoute extends RouteBuilder {
+
+  void configure(){
+
+    from("direct:processZip")
+        .routeId("processZip")
+        .split(new ZipSplitter()).streaming()
+        .to("file:facturas")
+        .to("log:unzipped?showAll=true&multiline=true&showFiles=true")
+  }
+}
