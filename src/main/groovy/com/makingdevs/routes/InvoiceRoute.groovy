bf13c0d7c6028b3eb3fe17e858edafd1e89f7b1e
neodevelop <juan@makingdevs.com>
2017-04-14 17:32:30 -0500

    Adding the splitting and handling attachments

--- src/main/groovy/com/makingdevs/routes/InvoiceRoute.groovy
+++ src/main/groovy/com/makingdevs/routes/InvoiceRoute.groovy
@@ -2,8 +2,8 @@ package com.makingdevs.routes
 
 import groovy.transform.CompileStatic
 import org.apache.camel.Exchange
-import org.apache.camel.Predicate
 import org.apache.camel.builder.RouteBuilder
+import org.apache.camel.component.mail.SplitAttachmentsExpression
 
 import javax.activation.DataHandler
 
@@ -12,6 +12,31 @@ class InvoiceRoute extends RouteBuilder {
 
   void configure() {
     from("direct:processWithAttachments")
+    .split(new SplitAttachmentsExpression())
+    .process { Exchange exchange ->
+      Map<String, DataHandler> attachments = exchange.in.attachments
+      String filename = ""
+      DataHandler dh = null
+      attachments.each { String f, DataHandler h ->
+        filename = f
+        dh = h
+      }
+
+      byte[] data =  exchange.context.typeConverter.convertTo(dh.inputStream.bytes.class, dh.inputStream) as byte[]
+
+      new FileOutputStream(filename).withStream {
+        it.write(data)
+      }
+
+      exchange.out.setBody(data, data.class)
+      Map headers = exchange.in.headers
+      headers.put("CamelFileName", filename) // FILENAME!!!!
+      exchange.out.setHeaders(headers)
+    }
+    .to("log:processWitAttachments?showBody=false&showHeaders=true&showFiles=true")
+    //.to("direct:storeInLocal")
+
+    from("direct:storeInLocal")
     .recipientList(simple('file:facturas/${header.expeditionYear}/${header.expeditionMonth}/'))
   }
 
