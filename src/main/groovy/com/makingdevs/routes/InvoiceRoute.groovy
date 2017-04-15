fdff30b6612b9fe30603c08252abdfe7e388400a
neodevelop <juan@makingdevs.com>
2017-04-14 19:58:26 -0500

    External path in config, based in config

--- src/main/groovy/com/makingdevs/routes/InvoiceRoute.groovy
+++ src/main/groovy/com/makingdevs/routes/InvoiceRoute.groovy
@@ -1,5 +1,6 @@
 package com.makingdevs.routes
 
+import com.makingdevs.config.Application
 import com.makingdevs.routes.utils.ProcessAttachments
 import groovy.transform.CompileStatic
 import org.apache.camel.LoggingLevel
@@ -9,17 +10,24 @@ import org.apache.camel.component.mail.SplitAttachmentsExpression
 @CompileStatic
 class InvoiceRoute extends RouteBuilder {
 
+  ConfigObject configuration = Application.instance.configuration
+
+  String fileEndpoint = """\
+      file:${configuration.get("file")["localPath"]}/\${header.expeditionYear}/\${header.expeditionMonth}/
+    """.trim()
+
   void configure() {
 
     errorHandler(loggingErrorHandler("com.makingdevs.invoice").level(LoggingLevel.ERROR))
 
     from("direct:processWithAttachments")
-    .split(new SplitAttachmentsExpression())
-    .process(new ProcessAttachments())
-    .to("direct:storeInLocal")
+        .split(new SplitAttachmentsExpression())
+        .process(new ProcessAttachments())
+        .to("direct:storeInLocal")
 
     from("direct:storeInLocal")
-    .recipientList(simple('file:facturas/${header.expeditionYear}/${header.expeditionMonth}/'))
+        .recipientList(simple(fileEndpoint))
+        .to("log:finalFile?showBody=false")
   }
 
 }
