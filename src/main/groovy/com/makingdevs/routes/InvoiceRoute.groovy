225f15cdfbe0ac4017bbbe256cdd64dec11ea562
neodevelop <juan@makingdevs.com>
2017-04-18 11:02:49 -0500

    Sending msg to notify process

--- src/main/groovy/com/makingdevs/routes/InvoiceRoute.groovy
+++ src/main/groovy/com/makingdevs/routes/InvoiceRoute.groovy
@@ -4,6 +4,7 @@ import com.makingdevs.config.Application
 import com.makingdevs.routes.utils.ProcessAttachments
 import com.makingdevs.routes.utils.UtilsForRoutes
 import groovy.transform.CompileStatic
+import org.apache.camel.Exchange
 import org.apache.camel.LoggingLevel
 import org.apache.camel.builder.RouteBuilder
 import org.apache.camel.component.aws.s3.S3Constants
@@ -30,6 +31,17 @@ class InvoiceRoute extends RouteBuilder {
     // TODO: Calculate the body size
         .setHeader(S3Constants.KEY, simple('${header.expeditionYear}/${header.expeditionMonth}/${in.header.CamelFileName}'))
         .to(s3Endpoint)
+        .process { Exchange e ->
+          String message = """\
+                            Archivo procesado: ${e.in.headers['CamelAwsS3Key']}.\
+                            \nAsunto: ${e.in.headers['Subject']}\
+                            \nRemitente: ${e.in.headers['Reply-To'] ?: 'Sin información'}\
+                          """.trim()
+          e.out.setBody(message, String)
+          e.out.setHeaders(e.in.headers)
+        }
+        // TODO: Use an aggregator to send one message
+        .to("telegram:bots/${configuration.get('telegram')['token']}?chatId=${configuration.get('telegram')['chatId']}")
   }
 
 }
