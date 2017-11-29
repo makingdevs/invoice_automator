37a0d90253c8fc7f9ac1c6aafcb0e05f83c68ecc
Antonio Reyes <antonio@makingdevs.com>
2017-11-29 11:34:23 -0600

    #4 adding tax detail in telegram message

--- src/main/groovy/com/makingdevs/routes/utils/ProcessAttachments.groovy
+++ src/main/groovy/com/makingdevs/routes/utils/ProcessAttachments.groovy
@@ -4,7 +4,9 @@ import org.apache.camel.Exchange
 import org.apache.camel.Processor
 
 import javax.activation.DataHandler
-
+import java.io.InputStream
+import java.io.BufferedReader
+import java.lang.StringBuilder
 /**
  * Created by neodevelop on 14/04/17.
  */
@@ -25,7 +27,39 @@ class ProcessAttachments implements Processor {
     exchange.out.setBody(data, data.class)
     Map headers = exchange.in.headers
     headers.put("CamelFileName", filename) // FILENAME!!!!
+    if(filename.endsWith(".xml")){
+        headers.putAll(pullDetail(data))
+    }
     exchange.out.setHeaders(headers)
 
+  }
+
+  private Map pullDetail(byte[] data){
+      Map detail = new  HashMap()
+      InputStream myInputStream = new ByteArrayInputStream(data)
+      BufferedReader inputReader = new BufferedReader(new InputStreamReader(myInputStream))
+      StringBuilder sb = new StringBuilder()
+      String inline = ""
+      while ((inline = inputReader.readLine()) != null) {
+        sb.append(inline)
+      }
+      String xml = sb.toString().toLowerCase()
+      def xmlParsed = new XmlParser().parseText(xml)
+      def cfdiNs = new groovy.xml.Namespace("http://www.sat.gob.mx/cfd/3", "cfdi")
+      
+      def emitter = xmlParsed[cfdiNs.emisor].@nombre
+      detail.put("emitter", emitter[0])
+      
+      detail.put("expeditionDate", xmlParsed.@fecha)
+
+      String concepts = ""
+      xmlParsed[cfdiNs.conceptos][cfdiNs.concepto].each { concepts += it.@descripcion +", "}
+      detail.put("concepts", concepts)
+
+      detail.put("amount", xmlParsed.@total)
+
+      def taxes = xmlParsed[cfdiNs.impuestos][cfdiNs.traslados][cfdiNs.traslado].@importe
+      detail.put("taxes", taxes[0])
+      detail
   }
 }
