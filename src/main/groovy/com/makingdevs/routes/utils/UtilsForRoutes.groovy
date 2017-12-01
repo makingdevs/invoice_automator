995b9c8e1674cb0ec5a3dba43a46981d4b594b35
Antonio Reyes <antonio@makingdevs.com>
2017-12-01 16:33:59 -0600

    #3 creating extractExpeditionDateMonth fuctions

--- src/main/groovy/com/makingdevs/routes/utils/UtilsForRoutes.groovy
+++ src/main/groovy/com/makingdevs/routes/utils/UtilsForRoutes.groovy
@@ -33,6 +33,19 @@ class UtilsForRoutes {
     "${indexMonth + 1}_${months.get(indexMonth)}"
   }
 
+  static String extractExpeditionDateMonth(@Headers Map headers) {
+    println "extractExpeditionDateMonth"
+    Locale.setDefault(Locale.US)
+    println "1"
+    String pattern = "EEE, d MMM yyyy HH:mm:ss Z"
+    String textDate = headers["expeditionDate"]
+    println textDate
+    if(textDate ==~ /\d{4}-\d{2}-\d{2}t\d{2}:\d{2}:\d{2}/)
+      pattern = "yyyy-MM-dd't'HH:mm:ss"
+    Integer indexMonth = new Date().parse(pattern, textDate as String)[Calendar.MONTH]
+    "${indexMonth + 1}_${months.get(indexMonth)}"
+  }
+  
   static Integer extractYearInvoice(@Headers Map headers) {
     Locale.setDefault(Locale.US)
     String pattern = "EEE, d MMM yyyy HH:mm:ss Z"
