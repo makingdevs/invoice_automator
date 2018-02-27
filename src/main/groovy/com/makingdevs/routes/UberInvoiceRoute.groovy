6f066b92b664150f9dbe1d2ec78e8ce1c154e1ea
cggg88jorge <jorge@makingdevs.com>
2018-02-27 14:23:48 -0600

    coding #5 super refactor del route para el manejo de errores

--- src/main/groovy/com/makingdevs/routes/UberInvoiceRoute.groovy
+++ src/main/groovy/com/makingdevs/routes/UberInvoiceRoute.groovy
@@ -1,12 +1,16 @@
 package com.makingdevs.routes
 
 import com.makingdevs.routes.utils.ProcessInvoiceDetail
+import com.makingdevs.routes.utils.ProcessLinkUber
+import com.makingdevs.config.Application
 import groovy.transform.CompileStatic
 import org.apache.camel.Exchange
 import org.apache.camel.LoggingLevel
 import org.apache.camel.builder.RouteBuilder
 import java.util.ArrayList
 import java.util.regex.Matcher
+import com.makingdevs.exception.NoLinkException
+import com.makingdevs.exception.RemovedLinkException
 
 /**
  * Created by makingdevs on 3/22/17.
@@ -14,44 +18,46 @@ import java.util.regex.Matcher
 @CompileStatic
 class UberInvoiceRoute extends RouteBuilder {
 
+  ConfigObject configuration = Application.instance.configuration
+
   void configure() {
 
-    errorHandler(loggingErrorHandler("com.makingdevs.uber").level(LoggingLevel.ERROR))
+    onException(NoLinkException)
+    .process({ Exchange exchange ->
+      String message = """\
+        No se puede procesar esta factura.
+        No se encontraron los links
+      """.toString()
+      exchange.out.setBody(message, String)
+    })
+    .handled(true)
+    .to("telegram:bots/${configuration.get('telegram')['token']}?chatId=${configuration.get('telegram')['chatId']}")
+
+    onException(RemovedLinkException)
+    .process({ Exchange exchange ->
+      String message = """\
+        No se puede procesar esta factura.
+        Los links regresan un codigo de status diferente a 200
+      """.toString()
+      exchange.out.setBody(message, String)
+    })
+    .handled(true)
+    .to("telegram:bots/${configuration.get('telegram')['token']}?chatId=${configuration.get('telegram')['chatId']}")
 
     from("direct:uberInvoice")
     .routeId("uberInvoice")
     .process({ Exchange exchange ->
       String msg = exchange.in.getBody(String)
-      String regex = /{1}http:\/\/email.uber.com\/.*{1}(?=\>)/
+      String regex = /https?:\/\/email.uber.com\/wf\/click.*{1}(?=\>)/
       Matcher matcher = msg =~ regex
       if(matcher.size() > 0){
         exchange.out.setHeaders(exchange.in.headers)
         exchange.out.setBody(matcher[0..1])
       }else{
-        println "there are not links to download files"
+        throw new NoLinkException("There are not links to download files")
       }
     }).split(body())
-    .process({ Exchange exchange ->
-      String link = exchange.in.getBody(String)
-      link = link.replace("http:", "http4:")
-      exchange.out.setHeaders(exchange.in.headers)
-      exchange.out.setBody(link)
-    })
-    .toD('${body}') 
-    .process({ Exchange exchange ->
-      byte[] data = exchange.in.getBody(byte[])
-      exchange.out.setBody(data, data.class)
-      
-      Map headers = exchange.in.headers
-      String regex = /"(.*?)"/
-      Matcher matcher = headers['Content-Disposition'] =~ regex
-      if(matcher.size() > 0){ 
-        headers.put("CamelFileName", ((ArrayList)matcher[0]).get(1)) // FILENAME!!!!
-        exchange.out.setHeaders(headers)
-      }else{
-        println "no match"
-      }
-    })
+    .process(new ProcessLinkUber())
     .process(new ProcessInvoiceDetail())
     .to("direct:storeInLocal")
     //.to("direct:processZip")
